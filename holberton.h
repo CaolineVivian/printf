f8b9a42a9f32eb70221efd798ad2f916943b344d
Bryan Alango <b.alango@launchpad.alx.app>
2020-10-26 19:11:51 +0300

    Updated header file

--- holberton.h
+++ holberton.h
@@ -1,7 +1,15 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <limits.h>
+
+
 
 int _printf(const char *format, ...);
 
-#endif /* HOLBERTON_H */
+#endif
