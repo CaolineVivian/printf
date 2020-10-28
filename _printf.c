327698177a3c18693afe6d12c03aff46fd070f2c
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-10-28 08:00:17 +0100

    add file _printf

--- _printf.c
+++ _printf.c
@@ -1,48 +1,37 @@
 #include "holberton.h"
 
 /**
- * _printf - Printf Function
- * @format: input constant
- * Return: size of bufer on success and -1 when fail.
+ * _printf - Receives the main string and all the necessary parameters to
+ * print a formated string
+ * @format: A string containing all the desired characters
+ * Return: A total count of the characters printed
  */
-
 int _printf(const char *format, ...)
 {
-	if (format != NULL)
-	{
-		va_list argument;
-		unsigned int a;
+	int printed_chars;
+	conver_t f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
+		{NULL, NULL}
+	};
+	va_list arg_list;
 
-		char *buffer, *temp_string;
+	if (format == NULL)
+		return (-1);
 
-		va_start(argument, format);
-		buffer = _calloc(2048, sizeof(char));
-
-		if (buffer == NULL)
-			return (-1);
-
-		a = 0;
-		while (format && format[a] != 00)
-		{
-			if (format[0] == 37 && format[a] == 00)
-			{
-				return (-1);
-			}
-			a = string_conct(buffer, format, a);
-			if (format[a] == 37)
-			{
-				a++;
-				temp_string = container(format[a], argument);
-				string_cat(buffer, temp_string);
-			}
-			if (format[a] != '\0')
-				a++;
-		}
-		a = string_len(buffer);
-		write(1, buffer, a);
-		va_end(argument);
-		free(buffer);
-		return (a);
-	}
-	return (-1);
+	va_start(arg_list, format);
+	/*Calling parser function*/
+	printed_chars = parser(format, f_list, arg_list);
+	va_end(arg_list);
+	return (printed_chars);
 }
